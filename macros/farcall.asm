1ebc2352afaf480752d6f7e04d2afeda677c988d
Tom Choi <jw95@live.cn>
2023-07-18 14:48:49 -0700

    save ROM0 space

--- macros/farcall.asm
+++ macros/farcall.asm
@@ -1,13 +1,15 @@
 MACRO farcall
 	ld b, BANK(\1)
 	ld hl, \1
-	call Bankswitch
+	; call Bankswitch
+	rst _Bankswitch
 ENDM
 
 MACRO callfar
 	ld hl, \1
 	ld b, BANK(\1)
-	call Bankswitch
+	; call Bankswitch
+	rst _Bankswitch
 ENDM
 
 MACRO farjp
